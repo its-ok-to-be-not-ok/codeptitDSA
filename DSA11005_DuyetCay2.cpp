69db6053afd8e1d30f4b20c2e59a85e6c043927b
Hoàng Trung Hiếu <87538191+its-ok-to-be-not-ok@users.noreply.github.com>
2023-01-08 11:52:09 +0700

    Add files via upload

---
+++ DSA11005_DuyetCay2.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    int val;
+    node *left, *right;
+    node(int v)
+    {
+        val = v;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+
+node *treebuild(int inOrder[], int levelOrder[], int n)
+{
+    int levelIndex = 0;
+
+    map<int, int> mp;
+    for(int i = 0; i < n; ++i)
+    {
+        mp[inOrder[i]] = i;
+    }
+
+    queue<pair<node*, pair<int, int>>> q;
+    node *root = new node(levelOrder[levelIndex++]);
+    q.push({root, {0, n - 1}});
+
+    while(!q.empty())
+    {
+        int m = q.size();
+        for(int i = 0; i < m; ++i)
+        {
+            auto X = q.front();
+            q.pop();
+            int leftBound = mp[X.first->val] - 1;
+            int rightBound = mp[X.first->val] + 1;
+            int leftBorder = X.second.first;
+            int rightBorder = X.second.second;
+            if(leftBorder <= leftBound)
+            {
+                X.first->left = new node(levelOrder[levelIndex++]);
+                q.push({X.first->left, {leftBorder, leftBound}});
+            }
+            if(rightBound <= rightBorder)
+            {
+                X.first->right = new node(levelOrder[levelIndex++]);
+                q.push({X.first->right, {rightBound, rightBorder}});
+            }
+        }
+    }
+    return root;
+}
+
+void postorder(node *root)
+{
+    if(root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->val << ' ';
+}
+
+void deleteTree(node *root)
+{
+    if(root == NULL) return;
+    deleteTree(root->left);
+    deleteTree(root->right);
+    delete root;
+}
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n; cin >> n;
+        int inOrder[n], levelOrder[n];
+        for(int i = 0; i < n; ++i) cin >> inOrder[i];
+        for(int i = 0; i < n; ++i) cin >> levelOrder[i];
+        node *root = treebuild(inOrder, levelOrder, n);
+        postorder(root);
+        cout << endl;
+        deleteTree(root);
+    }
+}
